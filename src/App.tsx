b92db9f8afd60bcc194a59c9945b837a7ba944ba
jrariza1 <ricardo.ariza@axissalud.com>
2026-04-08 15:10:42 -0500

    feat: implement dynamic layout resolver and sync infrastructure

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { usePartnerConfig } from "./hooks/usePartnerConfig";
-import { ClassicLayout } from "./components/landing/templates/classic/ClassicLayout";
-import { LoadingScreen } from "./components/LoadingScreen";
+import { LandingResolver } from "./components/LandingResolver";
 
 const queryClient = new QueryClient();
 
 const LandingApp = () => {
   const { config, isLoading } = usePartnerConfig();
-
-  if (isLoading) {
-    return <LoadingScreen />;
-  }
-
+  if (isLoading) return null;
   return (
     <ThemeProvider config={config}>
       <Routes>
-        <Route path="/" element={<ClassicLayout config={config} />} />
-        <Route path="/:slug" element={<ClassicLayout config={config} />} />
+        <Route path="/" element={<LandingResolver config={config} />} />
+        <Route path="/:slug" element={<LandingResolver config={config} />} />
       </Routes>
     </ThemeProvider>
   );
@@ -30,13 +25,9 @@ const LandingApp = () => {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <LandingApp />
-      </BrowserRouter>
+      <Toaster /><Sonner />
+      <BrowserRouter><LandingApp /></BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
-
 export default App;
